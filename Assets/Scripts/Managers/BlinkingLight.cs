98097fe32dda6facbe80abf328a2a90eb574d641
BenoitBellegarde <b.bellegarde@geomatika.fr>
2019-08-15 15:52:57 +0200

    Adding hat handler + Blinking light on objectives + Watson image on tv screens

---
+++ Assets/Scripts/Managers/BlinkingLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour
+{
+    private Light blinkingLight;
+    public float duration = 0.5f;     // The total of seconds the flash wil last
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        blinkingLight = GetComponent<Light>();
+
+        StartCoroutine(FlashNow());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator FlashNow()
+    {
+        while (true)
+        {
+            blinkingLight.enabled = !blinkingLight.enabled;
+            yield return new WaitForSeconds(duration);
+        }
+
+    }
+}
