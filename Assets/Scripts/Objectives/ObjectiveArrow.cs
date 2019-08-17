cbd731bc6c1a1a577be7ae985b56665192017ba7
BenoitBellegarde <b.bellegarde@geomatika.fr>
2019-08-17 18:54:11 +0200

    Adding new objectives + hats / Rebaking occlusion culling

--- Assets/Scripts/Objectives/ObjectiveArrow.cs
+++ Assets/Scripts/Objectives/ObjectiveArrow.cs
@@ -6,8 +6,9 @@ public class ObjectiveArrow : MonoBehaviour
 {
     public Vector3 position1 = new Vector3(0.0f, 41.5f, 0.0f);
     public Vector3 position2 = new Vector3(0.0f, 51.5f, 0.0f);
+    public float speed = 15f;
 
-    Vector3 currentTargetDestination;
+    private Vector3 currentTargetDestination;
 
     public float distanceTolerance = 0.5f; //you can change the tolerance to whatever you need it to be
 
@@ -19,7 +20,7 @@ public class ObjectiveArrow : MonoBehaviour
 
     void Update()
     {
-        transform.localPosition = Vector3.MoveTowards(transform.localPosition, currentTargetDestination, 15 * Time.deltaTime);
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, currentTargetDestination, speed * Time.deltaTime);
 
         if (Vector3.Distance(transform.localPosition, currentTargetDestination) <= distanceTolerance)
         {
