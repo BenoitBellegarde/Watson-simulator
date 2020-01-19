0a0969bf1f684eb9e7a29a544d9bb721a90b3201
BenoitBellegarde <b.bellegarde@geomatika.fr>
2020-01-19 15:47:31 +0100

    Implementing Free Look Cinemachine camera instead of basic camera + Cutscene with Timeline

--- Assets/Scripts/Cinematic/CinematicTrigger.cs
+++ Assets/Scripts/Cinematic/CinematicTrigger.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CinematicTrigger : MonoBehaviour
 {
     public string cinematicName;
-    public CinematicManager cinematicManager; 
+    public CinematicManager cinematicManager;
+    private PlayableDirector playableDirector;
 
     private Dictionary<string, bool> cinematicDone = new Dictionary<string, bool>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        playableDirector = GetComponent<PlayableDirector>();
     }
 
     // Update is called once per frame
@@ -24,34 +26,35 @@ public class CinematicTrigger : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Player") && (!cinematicDone.ContainsKey(cinematicName) || cinematicDone[cinematicName] != true))
         {
+            playableDirector.Play();
+            cinematicDone[cinematicName] = true;
+            /*witch (cinematicName)
+             {
+                 case "Lvl1_Presentation":
+                     cinematicDone[cinematicName] = true;
+                     StartCoroutine(cinematicManager.Lvl1_Presentation());
+                     break;
 
-            switch (cinematicName)
-            {
-                case "Lvl1_Presentation":
-                    cinematicDone[cinematicName] = true;
-                    StartCoroutine(cinematicManager.Lvl1_Presentation());
-                    break;
+                 case "Lvl1_Obj1_Advice":
+                     cinematicDone[cinematicName] = true;
+                     StartCoroutine(cinematicManager.Lvl1_Obj1_Advice());
+                     break;
 
-                case "Lvl1_Obj1_Advice":
-                    cinematicDone[cinematicName] = true;
-                    StartCoroutine(cinematicManager.Lvl1_Obj1_Advice());
-                    break;
+                 case "Lvl1_Obj2_Advice":
+                     cinematicDone[cinematicName] = true;
+                     StartCoroutine(cinematicManager.Lvl1_Obj2_Advice());
+                     break;
 
-                case "Lvl1_Obj2_Advice":
-                    cinematicDone[cinematicName] = true;
-                    StartCoroutine(cinematicManager.Lvl1_Obj2_Advice());
-                    break;
+                 case "Lvl1_Obj3_Advice":
+                     cinematicDone[cinematicName] = true;
+                     StartCoroutine(cinematicManager.Lvl1_Obj3_Advice());
+                     break;
 
-                case "Lvl1_Obj3_Advice":
-                    cinematicDone[cinematicName] = true;
-                    StartCoroutine(cinematicManager.Lvl1_Obj3_Advice());
-                    break;
-
-                case "Lvl1_Obj4_Advice":
-                    cinematicDone[cinematicName] = true;
-                    StartCoroutine(cinematicManager.Lvl1_Obj4_Advice());
-                    break;
-            }
+                 case "Lvl1_Obj4_Advice":
+                     cinematicDone[cinematicName] = true;
+                     StartCoroutine(cinematicManager.Lvl1_Obj4_Advice());
+                     break;
+             }*/
         }
     }
 
